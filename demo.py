209cae037c4d56247a7979fc418da5df52558878
rqiao <volpato30@gmail.com>
2017-03-31 00:57:19 -0400

    evaluation

--- demo.py
+++ demo.py
@@ -9,78 +9,118 @@ import numpy as np
 
 classes_name =  ['bare soil', 'dead tree']
 
+def iou(boxes1, boxes2):
+    lu = np.maximum(boxes1[0:2], boxes2[0:2])
+    rd = np.minimum(boxes1[2:], boxes2[2:])
+    intersection = np.maximum(rd - lu, [0,0])
+    inter_square = intersection[0]*intersection[1]
+    square1 = (boxes1[2]-boxes1[0]) * (boxes1[3] - boxes1[1])
+    square2 = (boxes2[2]-boxes2[0]) * (boxes2[3] - boxes2[1])
+    return inter_square / (square1 + square2 + 1e-5 - inter_square)
 
-def process_predicts(predicts):
-  p_classes = predicts[0, :, :, 0:2]
-  C = predicts[0, :, :, 2:4]
-  coordinate = predicts[0, :, :, 4:]
+def process_predicts(predicts, p_threshold=0.01):
+    result = []
+    if len(predicts.shape) == 3:
+        predicts = predicts[np.newaxis, :]
+    p_classes = predicts[0, :, :, 0:2]
+    C = predicts[0, :, :, 2:4]
+    coordinate = predicts[0, :, :, 4:]
+    coordinate = np.reshape(coordinate, (7, 7, 2, 4))
+    p_classes = np.reshape(p_classes, (7, 7, 1, 2))
+    C = np.reshape(C, (7, 7, 2, 1))
 
-  p_classes = np.reshape(p_classes, (7, 7, 1, 2))
-  C = np.reshape(C, (7, 7, 2, 1))
+    P = C * p_classes
 
-  P = C * p_classes
+    index = np.argmax(P)
 
-  #print P[5,1, 0, :]
+    index = np.unravel_index(index, P.shape)
+    while(P[index] > p_threshold):
+        class_num = index[3]
+        max_coordinate = coordinate[index[0], index[1], index[2], :]
 
-  index = np.argmax(P)
+        xcenter = max_coordinate[0]
+        ycenter = max_coordinate[1]
+        w = max_coordinate[2]
+        h = max_coordinate[3]
+        if w < 0 or h < 0:
+            P[index]=0
+            index = np.argmax(P)
+            index = np.unravel_index(index, P.shape)
+            continue
+        xcenter = (index[1] + xcenter) * (448/7.0)
+        ycenter = (index[0] + ycenter) * (448/7.0)
 
-  index = np.unravel_index(index, P.shape)
+        w = w * 448
+        h = h * 448
 
-  class_num = index[3]
+        xmin = xcenter - w/2.0
+        ymin = ycenter - h/2.0
 
-  coordinate = np.reshape(coordinate, (7, 7, 2, 4))
+        xmax = xmin + w
+        ymax = ymin + h
+        result.append((xmin, ymin, xmax, ymax, class_num, P[index]))
+        
+        P[index]=0
+        index = np.argmax(P)
+        index = np.unravel_index(index, P.shape)
+    return result
 
-  max_coordinate = coordinate[index[0], index[1], index[2], :]
+def _one_step_suppression(box_list, iou_threshold):
+    remove_list = []
+    for box in box_list[1:]:
+        if iou(box_list[0][:4], box[:4]) >= iou_threshold:
+            remove_list.append(box)
+    box_list = [b for b in box_list if b not in remove_list]
+    return box_list
 
-  xcenter = max_coordinate[0]
-  ycenter = max_coordinate[1]
-  w = max_coordinate[2]
-  h = max_coordinate[3]
+def non_maximum_suppression(box_list, iou_threshold):
+    if len(box_list) <= 1:
+        return box_list
+    output = []
+    while(True):
+        box_list = _one_step_suppression(box_list, iou_threshold)
+        assert len(box_list) > 0
+        if len(box_list) > 1:
+            output.append(box_list[0])
+            box_list = box_list[1:]
+        else:
+            output.append(box_list[0])
+            return output
 
-  xcenter = (index[1] + xcenter) * (448/7.0)
-  ycenter = (index[0] + ycenter) * (448/7.0)
+if __name__ == "__main__":
+    common_params = {'image_size': 448, 'num_classes': 2, 
+                                    'batch_size':1}
+    net_params = {'cell_size': 7, 'boxes_per_cell':2, 'weight_decay': 0.0005}
 
-  w = w * 448
-  h = h * 448
+    net = YoloTinyNet(common_params, net_params, test=True)
 
-  xmin = xcenter - w/2.0
-  ymin = ycenter - h/2.0
+    image = tf.placeholder(tf.float32, (1, 448, 448, 3))
+    predicts = net.inference(image)
 
-  xmax = xmin + w
-  ymax = ymin + h
+    sess = tf.Session()
 
-  return xmin, ymin, xmax, ymax, class_num
-
-common_params = {'image_size': 448, 'num_classes': 2, 
-                'batch_size':1}
-net_params = {'cell_size': 7, 'boxes_per_cell':2, 'weight_decay': 0.0005}
-
-net = YoloTinyNet(common_params, net_params, test=True)
-
-image = tf.placeholder(tf.float32, (1, 448, 448, 3))
-predicts = net.inference(image)
-
-sess = tf.Session()
-
-np_img = cv2.imread('/home/rui/tensorflow-yolo/data/processed_data/test/D1019020201_ob78_crop1.JPG')
-resized_img = cv2.resize(np_img, (448, 448))
-np_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
+    np_img = cv2.imread('/home/rui/tensorflow-yolo/data/processed_data/test/D1019020201_ob77_crop0.JPG')
+    resized_img = cv2.resize(np_img, (448, 448))
+    np_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
 
 
-np_img = np_img.astype(np.float32)
+    np_img = np_img.astype(np.float32)
 
-np_img = np_img / 255.0 * 2 - 1
-np_img = np.reshape(np_img, (1, 448, 448, 3))
+    np_img = np_img / 255.0 * 2 - 1
+    np_img = np.reshape(np_img, (1, 448, 448, 3))
 
-saver = tf.train.Saver(net.trainable_collection)
-ckpt = tf.train.get_checkpoint_state('models/train/')
-saver.restore(sess,ckpt.model_checkpoint_path)
+    saver = tf.train.Saver(net.trainable_collection)
+    ckpt = tf.train.get_checkpoint_state('models/train/')
+    saver.restore(sess,ckpt.model_checkpoint_path)
 
-np_predict = sess.run(predicts, feed_dict={image: np_img})
+    np_predict = sess.run(predicts, feed_dict={image: np_img})
 
-xmin, ymin, xmax, ymax, class_num = process_predicts(np_predict)
-class_name = classes_name[class_num]
-cv2.rectangle(resized_img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 0, 255))
-cv2.putText(resized_img, class_name, (int(xmin), int(ymin)), 2, 1.5, (0, 0, 255))
-cv2.imwrite('test_out.jpg', resized_img)
-sess.close()
+    box_list = process_predicts(np_predict, 0.04)
+    box_list = non_maximum_suppression(box_list, 0.5)
+    for ob in box_list:
+        xmin, ymin, xmax, ymax, class_num, p = ob
+        class_name = classes_name[class_num]
+        cv2.rectangle(resized_img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 0, 255))
+        cv2.putText(resized_img, class_name + ' {:.2f}'.format(p), (int(xmin), int(ymin)), 2, 1.5, (0, 0, 255))
+    cv2.imwrite('test_out.jpg', resized_img)
+    sess.close()
---
+++ evaluation.py
@@ -0,0 +1,118 @@
+import sys
+sys.path.append('./')
+import tensorflow as tf
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from yolo.net.yolo_tiny_net import YoloTinyNet
+from yolo.dataset.text_dataset import TextDataSet
+from demo import *
+# in total we have 714 test images
+
+class TextDataSetV2(TextDataSet):
+    def __init__(self, common_params, dataset_params):
+        super(TextDataSetV2, self).__init__(common_parameters, dataset_params)
+    def record_process(self, record):
+        image = cv2.imread(record[0])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (self.height, self.width))
+        labels = [[0, 0, 0, 0, 0]] * self.max_objects
+        i = 1
+        object_num = 0
+        while i < len(record):
+            xmin = record[i]
+            ymin = record[i + 1]
+            xmax = record[i + 2]
+            ymax = record[i + 3]
+            class_num = record[i + 4]
+            labels[object_num] = [xmin, ymin, xmax, ymax, class_num]
+            object_num += 1
+            i += 5
+            if object_num >= self.max_objects:
+                break
+        return [image, labels, object_num]
+
+common_parameters = {'image_size': 448, 'batch_size': 16,\
+    'num_classes': 2, 'max_objects_per_image': 10}
+dataset_parameters = {'path': 'data/processed_data/test_label.txt', 'thread_num': 5}
+dataset =  TextDataSetV2(common_parameters, dataset_parameters)
+classes_name = ['bare soil', 'dead tree']
+net_params = {'cell_size': 7, 'boxes_per_cell':2, 'weight_decay': 0.0005}
+batch_size = common_parameters['batch_size']
+net = YoloTinyNet(common_parameters, net_params, test=True)
+image = tf.placeholder(tf.float32, (None, 448, 448, 3))
+predicts = net.inference(image)
+sess = tf.Session()
+
+num_imgs = 714 // batch_size * batch_size
+predicted_boxes = np.zeros((num_imgs, net_params['cell_size'], net_params['cell_size'], \
+    12), dtype=np.float32)
+ground_truth_boxes = []
+for i in range(714 // batch_size):
+    images, labels, objects_num = dataset.batch()
+    saver = tf.train.Saver(net.trainable_collection)
+    ckpt = tf.train.get_checkpoint_state('models/train/')
+    saver.restore(sess,ckpt.model_checkpoint_path)
+    np_predict = sess.run(predicts, feed_dict={image: images})
+    assert np_predict.shape[-1] == 12
+    predicted_boxes[i * batch_size:(i+1) * batch_size] = np_predict
+    labels = list(labels)
+    for i in range(batch_size):
+        temp = labels[i][:objects_num[i],:]
+        ground_truth_boxes.append(temp[temp[:,4]==0]) # only consider boxes for dead tree class
+
+def compute_precision_and_recall(predicted_boxes, ground_truth_boxes, p_threshold=0.1,\
+        nms_threshold=0.5, true_iou_threshold=0.3):
+    processed_boxes = []
+    for i in range(len(predicted_boxes)):
+        predicted_result = process_predicts(predicted_boxes[i], p_threshold)
+        predicted_result = [tup for tup in predicted_result if tup[4]==0]
+        nms_result = non_maximum_suppression(predicted_result, nms_threshold)
+        processed_boxes.append(nms_result)
+    # compute recall 
+    r_tp = 0
+    fn = 0
+    for i in range(len(ground_truth_boxes)):
+        for true_box in ground_truth_boxes[i]:
+            fn += 1
+            for predict_box in processed_boxes[i]:
+                if iou(true_box[:4], predict_box[:4]) >= true_iou_threshold:
+                    r_tp += 1
+                    fn -= 1
+                    break
+    recall = r_tp / float(r_tp + fn)
+    # compute precision
+    p_tp = 0
+    fp = 0
+    for i in range(len(processed_boxes)):
+        for predict_box in processed_boxes[i]:
+            fp += 1
+            for true_box in ground_truth_boxes[i]:
+                if iou(true_box[:4], predict_box[:4]) >= true_iou_threshold:
+                    p_tp += 1
+                    fp -= 1
+                    break
+    if p_tp + fp > 0:
+        precision = p_tp / float(p_tp + fp)
+    else:
+        precision = 1
+    print('p_tp: {}\tr_tp: {}\tfn: {}\tfp: {}'.format(p_tp, r_tp, fn, fp))
+    return precision, recall
+
+thres_grid = np.arange(1e-5, 0.2, 1e-3)
+precision_list = []
+recall_list = []
+for p_threshold in thres_grid:
+    precision, recall = compute_precision_and_recall(predicted_boxes, ground_truth_boxes, p_threshold)
+    precision_list.append(precision)
+    recall_list.append(recall)
+precision = np.asarray(precision_list)
+recall = np.asarray(recall_list)
+np.savez('precision_recall.npz', thres_grid, precision, recall)
+keep = precision.argmax()
+plt.plot(recall[:keep], precision[:keep])
+plt.xlabel('recall rate')
+plt.ylabel('precision rate')
+plt.savefig('precision_recall_curve.png')
